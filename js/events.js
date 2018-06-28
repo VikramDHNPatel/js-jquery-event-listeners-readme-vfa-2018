3e26b28d79bd05695f808c5b77ad4297c747fa6d
Vikram Patel <vikramdhnpatel@gmail.com>
2018-06-28 00:24:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,30 +1,5 @@
 //define functions here
 
-function getIt() {
-  $("p").on("click", function() {
-    alert("Hey!");
-  });
-}
-
-function frameIt() {
-  $('img').on('load', function() {
-    $('img').addClass('tasty');
-  });
-}
-
-function pressIt() {
-  $("input#typing").on('keydown', function(key) {
-    if (key.keyCode == 71) {
-      alert('g was pressed');
-    }
-  });
-} 	 
-}); 	function submitIt() {
-  $('form').on('submit', function() {
-    alert("your form is going to be submitted now");
-    return;
-  });
-}
 
 $(document).ready(function(){
 
@@ -38,4 +13,3 @@ pressIt();
 
 submitIt();
 
-});
